620ebe13fe3a51ea8757274881dba0d11c8fe794
anubhavshrivastav <shrivastavanubhav0@gmail.com>
2025-09-04 19:21:58 +0530

    remove cors

--- src/index.js
+++ src/index.js
@@ -11,13 +11,7 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin:[
-    "http://localhost:5173",
-    "https://cashkaroweb.netlify.app"
-  ],
-  credentials: true,
-}));
+app.use(cors());
 
 dotenv.config({
   path: "./.env",
